5d50e8d80e9b83d2a27a2d093b390895c5bcd1f6
santi <santibrad2215@hotmail.com>
2023-01-18 23:36:50 -0600

    Added more flasks routes

--- app.py
+++ app.py
@@ -16,3 +16,38 @@ def login():
 def register():
     return render_template('register.html')
 
+@app.route('/page/<string:page_name>')
+def render_static(page_name):
+    return render_template('%s.html' % page_name)
+
+def charts():
+    return render_template('charts.html')
+
+def Error404():
+    return render_template('404.html')
+
+def buttons():
+    return render_template('buttons.html')
+
+def cards():
+    return render_template('cards.html')
+
+def forgotpass():
+    return render_template('forgot-password.html')
+
+# Utilities    
+
+def animations():
+    return render_template('utilities-animation.html')
+
+def borders():
+    return render_template('utilities-border.html')
+
+def color():
+    return render_template('utilities-color.html')
+
+def animations():
+    return render_template('utilities-other.html')
+
+
+
